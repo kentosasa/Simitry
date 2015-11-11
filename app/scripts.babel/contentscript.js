590e789e47f1816ffed9340459ac254d61092e18
kento sasamoto <knote0721@gmail.com>
2015-11-11 10:11:54 +0900

    動作確認

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -22,7 +22,7 @@ chrome.extension.sendRequest({url: location.href}, function(res){
 var EntryBox = React.createClass({
   getInitialState() {
     return {
-      position: 15,
+      position: 0,
       data: this.props.res
     };
   },
@@ -34,37 +34,35 @@ var EntryBox = React.createClass({
   },
   render: function() {
     var footer = [];
-    var max = this.state.data.length-1;
+    var max = this.state.data.length;
     var data = this.state.data;
     var style = {};
     console.log("data/" + data.length + " position/" + this.state.position);
     style.color = '#0f0f0f';
     for (var i = this.state.position - 2; i <= this.state.position + 2; i++) {
-      var val = data[i];
-      // footer.push(<div key={i} className={(i < 0 || i > max) ? 'block1 hide' : 'block1'}>{i}</div>);
-      if (i < 0 || i > max) {
-        footer.push(<a key={i} href="#" className='entry-box hide'></a>)
-      } else {
-        var entryStyle = {
-          background: 'url(' + val.screenshot + ')'
-        };
-        var d = new Date(val.created_at);
-        footer.push(
-          <a key={i} href={val.link} className={this.state.position === i ? 'entry-box select' : 'entry-box'} style={entryStyle}>
-            <div className="entry-info">
-              <div className="entry-contents">
-                <div className="title">{val.title}</div>
-                <div className="description">{val.description}</div>
-              </div>
-              <div className="entry-meta">
-                <span className="entry-count">Count: {val.count + ""}</span>
-                <span className="created_at">{d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate()}</span>
-              </div>
+      var position = i
+      if (i < 0) position = i + max;
+      if (i >= max) position = i - max;
+      var val = data[position];
+      var entryStyle = {
+        background: 'url(' + val.screenshot + ')'
+      };
+      var d = new Date(val.created_at);
+      footer.push(
+        <a key={position} href={val.link} className={this.state.position === position ? 'entry-box entry-select' : 'entry-box'} style={entryStyle}>
+          <div className="entry-info">
+            <div className="entry-contents">
+              <div className="title">{val.title}</div>
+              <div className="description">{val.description}</div>
             </div>
-          </a>
-          );
-      }
-    };
+            <div className="entry-meta">
+              <span className="entry-count">Count: {val.count + ""}</span>
+              <span className="created_at">{d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate()}</span>
+            </div>
+          </div>
+        </a>
+        );
+      };
     return (
     <div className="simility-footer-contents">
       {footer}
@@ -72,11 +70,14 @@ var EntryBox = React.createClass({
     );
   },
   handleKeydown(e) {
+    console.log(this.state.position);
     console.log(e.keyCode);
     if (e.keyCode == 37) {
-      if (this.state.position > 0) this.setState({position: this.state.position - 1});
+      if (this.state.position == 0) { this.setState({position: this.state.position + this.state.data.length}); }
+      else if (this.state.position > 0) this.setState({position: this.state.position - 1});
     } else if (e.keyCode == 39) {
-      if(this.state.position < this.state.data.length-1) this.setState({position: this.state.position + 1});
+      if(this.state.position == this.state.data.length) { this.setState({position: this.state.position - this.state.data.length}); }
+      else if (this.state.position < this.state.data.length) this.setState({position: this.state.position + 1});
     } else if (e.keyCode == 13){
       location.href = this.state.data[this.state.position].link;
     };
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -21,7 +21,7 @@ var EntryBox = React.createClass({
 
   getInitialState: function getInitialState() {
     return {
-      position: 15,
+      position: 0,
       data: this.props.res
     };
   },
@@ -33,59 +33,57 @@ var EntryBox = React.createClass({
   },
   render: function render() {
     var footer = [];
-    var max = this.state.data.length - 1;
+    var max = this.state.data.length;
     var data = this.state.data;
     var style = {};
     console.log("data/" + data.length + " position/" + this.state.position);
     style.color = '#0f0f0f';
     for (var i = this.state.position - 2; i <= this.state.position + 2; i++) {
-      var val = data[i];
-      // footer.push(<div key={i} className={(i < 0 || i > max) ? 'block1 hide' : 'block1'}>{i}</div>);
-      if (i < 0 || i > max) {
-        footer.push(React.createElement('a', { key: i, href: '#', className: 'entry-box hide' }));
-      } else {
-        var entryStyle = {
-          background: 'url(' + val.screenshot + ')'
-        };
-        var d = new Date(val.created_at);
-        footer.push(React.createElement(
-          'a',
-          { key: i, href: val.link, className: this.state.position === i ? 'entry-box select' : 'entry-box', style: entryStyle },
+      var position = i;
+      if (i < 0) position = i + max;
+      if (i >= max) position = i - max;
+      var val = data[position];
+      var entryStyle = {
+        background: 'url(' + val.screenshot + ')'
+      };
+      var d = new Date(val.created_at);
+      footer.push(React.createElement(
+        'a',
+        { key: position, href: val.link, className: this.state.position === position ? 'entry-box entry-select' : 'entry-box', style: entryStyle },
+        React.createElement(
+          'div',
+          { className: 'entry-info' },
           React.createElement(
             'div',
-            { className: 'entry-info' },
+            { className: 'entry-contents' },
             React.createElement(
               'div',
-              { className: 'entry-contents' },
-              React.createElement(
-                'div',
-                { className: 'title' },
-                val.title
-              ),
-              React.createElement(
-                'div',
-                { className: 'description' },
-                val.description
-              )
+              { className: 'title' },
+              val.title
             ),
             React.createElement(
               'div',
-              { className: 'entry-meta' },
-              React.createElement(
-                'span',
-                { className: 'entry-count' },
-                'Count: ',
-                val.count + ""
-              ),
-              React.createElement(
-                'span',
-                { className: 'created_at' },
-                d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate()
-              )
+              { className: 'description' },
+              val.description
+            )
+          ),
+          React.createElement(
+            'div',
+            { className: 'entry-meta' },
+            React.createElement(
+              'span',
+              { className: 'entry-count' },
+              'Count: ',
+              val.count + ""
+            ),
+            React.createElement(
+              'span',
+              { className: 'created_at' },
+              d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate()
             )
           )
-        ));
-      }
+        )
+      ));
     };
     return React.createElement(
       'div',
@@ -94,11 +92,16 @@ var EntryBox = React.createClass({
     );
   },
   handleKeydown: function handleKeydown(e) {
+    console.log(this.state.position);
     console.log(e.keyCode);
     if (e.keyCode == 37) {
-      if (this.state.position > 0) this.setState({ position: this.state.position - 1 });
+      if (this.state.position == 0) {
+        this.setState({ position: this.state.position + this.state.data.length });
+      } else if (this.state.position > 0) this.setState({ position: this.state.position - 1 });
     } else if (e.keyCode == 39) {
-      if (this.state.position < this.state.data.length - 1) this.setState({ position: this.state.position + 1 });
+      if (this.state.position == this.state.data.length) {
+        this.setState({ position: this.state.position - this.state.data.length });
+      } else if (this.state.position < this.state.data.length) this.setState({ position: this.state.position + 1 });
     } else if (e.keyCode == 13) {
       location.href = this.state.data[this.state.position].link;
     };
